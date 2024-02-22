ca3d480de60af62de3477ed2db06a378a4cba5ce
kaplunovruslan <kaplunov.80@inbox.ru>
2024-02-22 15:43:53 +0300

    initial

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+        String folderPAth = "J:/kaplu/Desktop/Skillbox/READ";
+        File file = new File(folderPAth);
+
+        System.out.println(getFolderSize(file));
+
+    }
+
+    public static long getFolderSize(File folder){
+        if (folder.isFile()){
+            return folder.length();
+        }
+
+        long sum = 0;
+        File[] files = folder.listFiles();
+        for (File file: files){
+            sum += getFolderSize(file);
+        }
+        return sum;
     }
 }
